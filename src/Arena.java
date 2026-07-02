cceb36e0902de66856f9f333cc1432b23bda6a53
oteste44 <oteste306@gmail.com>
2026-07-02 17:07:26 -0300

    rats

---
+++ src/Arena.java
@@ -0,0 +1,12 @@
+import rpg.Guerreiro;
+import rpg.Mago;
+
+public class Arena {
+    public static void main(String[] args) {
+        System.out.println("A batalha vai Começar!");
+        Guerreiro osama = new Guerreiro("osama",2001,119);
+        Mago patolino = new Mago("patolino", 2010, 8000);
+
+
+    }
+}
